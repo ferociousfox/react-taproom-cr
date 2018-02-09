15165d1ea452ee30d8379ad0c6596261aec84385
shane ryan <onyx.shane@gmail.com>
2018-02-09 10:33:42 -0800

    add code to finish App.jsx including css reset.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Error404 from './Error404';
 import Home from './Home';
+import KegList from './KegList';
+import Error404 from './Error404';
 
 //define Home!
 function App(){
   return (
-    <Switch>
-      <Route exact path='/' component={Home} />
-      <Route component={Error404} />
-    </Switch>
+    <div>
+      <style global jsx>{`
+        html, body, h1, h2, h3, p, ul, li{
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+          font-family: sans-serif;
+        }
+
+    `}</style>
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/keglist' component={KegList}/>
+        <Route component={Error404} />
+      </Switch>
+    </div>
   );
 }
 
